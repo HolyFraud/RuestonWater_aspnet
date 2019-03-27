1cd9e7cbc30b9f9af715e63e41b2a4d98fde4d49
Peng Shen <sp100809@gmail.com>
2019-03-28 01:06:53 +1100

    Lastest

---
+++ Ruestonwater/Admin/Login.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="Ruestonwater.Admin.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Login Platform</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Login</h1>
+            <asp:TextBox ID="tbaccount" runat="server" placeholder="Enter Account" Width="200px" Height="20px"></asp:TextBox><br /><br />
+            <asp:TextBox ID="tbpassword" runat="server" TextMode="Password" placeholder="Enter Password" Width="200px" Height="20px"></asp:TextBox><br /><br />
+            <asp:Button ID="loginbtn" runat="server" Text="Login" OnClick="loginbtn_Click"/>
+            <asp:Label ID="lblogin" runat="server" Text="Label" Visible="false"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Ruestonwater/Admin/Login.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ruestonwater.Admin
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void loginbtn_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["SQLConnectionString"].ConnectionString);
+            sqlConnection.Open();
+            string query = "select count(*) from ManagerList where UserName = '" + tbaccount.Text + "' And Password = '" + tbpassword.Text + "' And LastLogin = GETDATE()";
+            SqlCommand cmd = new SqlCommand(query, sqlConnection);
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                int count = Convert.ToInt32(reader[0]);
+                if (count == 1)
+                {
+                    Session["ManagerActive"] = 1;
+                    lblogin.Visible = true;
+                    lblogin.Text = "Login Successfully...!";
+                    Response.Redirect("/Admin/Index.aspx");
+                }
+                else
+                {
+                    Session.Remove("ManagerActive");
+                    lblogin.Visible = true;
+                    lblogin.Text = "Login failed, Plz try again...!";
+                }
+            }
+        }
+    }
+}
